cc1eeb33405fef04eacdc7fc5e9ac4c3c442a94d
Louis Charpentier <95610858+Clovelahuiss@users.noreply.github.com>
2024-01-29 17:03:13 +0100

    Update models.py

--- projet_info/app_info/models.py
+++ projet_info/app_info/models.py
@@ -18,18 +18,6 @@ class Reservation(models.Model):
     nombre_personnes = models.IntegerField()
     table = models.ForeignKey(Table, on_delete=models.SET_NULL, null=True)
 
-    def assign_table(self):
-        # Trouver les tables qui peuvent accueillir le groupe et qui ne sont pas déjà réservées à l'heure demandée
-        available_tables = Table.objects.filter(capacite__gte=self.nombre_personnes).exclude(
-            reservation__date_heure=self.date_heure
-        )
-        if available_tables:
-            self.table = available_tables.first()
-            self.save()
-        else:
-            # Gérer le cas où aucune table n'est disponible
-            # Par exemple, vous pouvez définir 'table' à None ou gérer cette situation d'une autre manière
-            self.table = None
     def __str__(self):
         return f"{self.table} - {self.date_heure.strftime('%Y-%m-%d %H:%M')}"
 
@@ -41,4 +29,9 @@ class Reservation(models.Model):
         if self.date_heure < timezone.now():
             raise ValidationError('La date et l\'heure ne peuvent pas être dans le passé')
 
-    
+        # Check if a table is available for the given number of people
+        available_tables = Table.objects.filter(capacite__gte=self.nombre_personnes, reservation__date_heure__lt=self.date_heure)
+        if available_tables.exists():
+            self.table = random.choice(available_tables)
+        else:
+            raise ValidationError('Désolé, aucune table n\'est disponible')
